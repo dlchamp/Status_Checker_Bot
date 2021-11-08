ae69aab3b9904dd8c716049dbe19953b32af9b4d
dlchamp <36091350+dlchamp@users.noreply.github.com>
2021-11-07 22:40:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 '''
-This is a simply bot designed for @Courier :envelope:
-As requested, this bot will simply run a status check at 1PM local time every day
+This is a simple bot designed for @Courier :envelope:
+As requested, this bot will run a status check at 1PM local time every day
 If the members' status == the STATUS_MESSAGE variable, a message is sent to the channel
 designated by the MESSAGE_CHANNEL variable
-'''
 
+Set your bot token in the .env-sample file, then rename the file to .env
+
+'''
 import os
 import datetime
 import nextcord
@@ -19,15 +21,12 @@ Input the status message you wish to check against for member custom status
 Must also be wrapped in ' '
 Input the time you wish for status check to run in hours:minutes (ex. 1:00) - 24 hour only
 '''
-GUILD_ID = '778812053980315649'
-MESSAGE_CHANNEL = '902733099673202730'
-STATUS_MESSAGE = 'discord.gg/wjmVRmjxXC'
-TIME = '13:00'
+GUILD_ID = 'YOUR GUILD ID'
+MESSAGE_CHANNEL = 'YOUR MESSAGE CHANNEL'
+STATUS_MESSAGE = 'discord.gg/wjmVRmjxXC' # Adjust this if you change your invite URL (always leave out the https:// part)
+TIME = '13:00'  # local time variable, use 24 hour time - default 1PM CST
 
-
-'''
-Should never have to adjust anything below this comment line.
-'''
+###### END USER CONFIG AREA #########
 
 intents = nextcord.Intents.all()
 bot = nextcord.Client(intents=intents)
@@ -54,6 +53,12 @@ async def on_ready():
 
 @tasks.loop(hours=24)
 async def presence_check():
+    '''
+    Run once a day, first run will happen at bot start.
+    When task runs, if the time is not the configured time, it will sleep
+    until that time appears again.  This is the main section of this bot
+    as it is the function that checks member' status for the keyword
+    '''
 
     # Seperate TIME into hours, minutes
     # Sleep until designated time
@@ -74,12 +79,7 @@ async def presence_check():
                         print(f'{member.name} | {a}')
                         activity = a.name
                         if STATUS_MESSAGE in activity:
-                            print('Status match')
                             await channel.send(f'{member.name} needs XP. Status set to our group.')
-                        else:
-                            print("Status mismatch")
-
-
 
 
 load_dotenv()
